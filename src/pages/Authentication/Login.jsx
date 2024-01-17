f3446a5e630c922fcd8ab1a31f98913913a47f1e
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-01-17 14:58:07 +0530

    Build login page

---
+++ src/pages/Authentication/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { ErrorMessage, Field, Form, Formik, validateYupSchema } from 'formik'
+import { Button, TextField } from '@mui/material'
+import * as Yup from "yup"
+
+
+const initialValues={email:"", password:""}
+const validationSchema={email: Yup.string().email("Invalid email").required("Email is required"), password: Yup.string().min(6, "Password must be atleast 6 characters").required("Password is required")}
+
+const Login = () => {
+  const[formValue, setFormValue] = useState();
+  const handleSubmit = (values) => {
+    console.log("Handle submit", values);
+  }
+  return (
+    <>
+      <Formik onSubmit={handleSubmit}
+      // validationSchema={validationSchema}
+      initialValues={initialValues}>
+        <Form className='space-y-5'>
+          <div className='space-y-5'>
+            <div>
+              <Field as={TextField} name="email" placeholder="Email" type="email" variant="outlined" fullWidth/>
+              <ErrorMessage name='email' component={"div"} className='text-red-500'/>
+            </div>
+
+            <div>
+              <Field as={TextField} name="password" placeholder="Password" type="password" variant="outlined" fullWidth/>
+              <ErrorMessage name='password' component= "div" className='text-red-500'/>
+            </div>
+
+            <Button sx={{padding: " .8rem 0rem"}} fullWidth type='submit' variant="contained" color='primary'>Login</Button>
+          </div>
+        </Form>
+      </Formik>
+    </>
+  )
+}
+
+export default Login
