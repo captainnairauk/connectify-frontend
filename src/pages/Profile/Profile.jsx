50ecdb7069d03ad4d2b796171e3cdd239a745244
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-01-22 11:56:58 +0530

    Build profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,79 @@
+import { Avatar, Box, Button, Card, Tab, Tabs } from '@mui/material'
 import React from 'react'
 import { useParams } from 'react-router-dom'
-
+import PostCard from '../../components/Post/PostCard';
+import UserReelCard from '../../components/Reels/UserReelCard';
+const tabs = [
+  {value:"post", name:"Post"},
+  {value:"reels", name:"Reels"},
+  {value:"saved", name:"Saved"},
+  {value:"repost", name:"Repost"},
+];
+const posts = [1,1,1,1];
+const reels = [1,1,1,1];
+const savedPost = [1,1,1,1];
 const Profile = () => {
-  const {id} = useParams()
+  const {id} = useParams();
+  const [value, setValue] = React.useState('post');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div>Profile - {id}</div>
+    <Card className='my-10 w-[70%]'>
+      <div className='rounded-md'>
+        <div className='h-[15rem]'>
+          <img className='w-full h-full rounded-t-md'
+          src="https://cdn.pixabay.com/photo/2014/01/13/20/01/pebbles-243910_1280.jpg" alt="pebbles on the beach" />
+        </div>
+        <div className='px-5 flex justify-between items-start mt-5 h-[5rem]'>
+          <Avatar className="transform -translate-y-24" sx={{width:"10rem", height:"10rem"}} src='https://cdn.pixabay.com/photo/2019/04/17/20/18/golden-retriever-4135265_1280.jpg'/>
+          {true? <Button sx={{borderRadius:"20px"}} variant='outlined'>Edit Profile</Button>: <Button variant='outlined'>Follow</Button>}
+        </div>
+        <div className='p-5'>
+          <div>
+            <h1 className='py-1 font-bold text-xl'>User name</h1>
+            <p>@username</p>
+          </div>
+          <div className='flex gap-5 items-center py-3'>
+            <span>41 posts</span>
+            <span>35 followers</span>
+            <span>5 followings</span>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <section>
+          <Box sx={{ width: '100%', borderBottom:1, borderColor:"divider"}}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="wrapped label tabs example"
+            >
+              {tabs.map((item)=> <Tab value={item.value} label={item.name} wrapped />)}
+            </Tabs>
+          </Box>
+          <div className='flex justify-center'>
+            {value === "post" ? (<div className='space-y-5 w-[70%] my-10'>
+              {posts.map((item) => (<div className='border border-slate-100 rounded-md'>
+                <PostCard/>
+              </div>))}
+
+            </div>):value==="reels"?<div className='flex justify-center flex-wrap gap-2 my-10'>
+              {reels.map((item) => <UserReelCard/>)}
+            </div> : value === "saved" ? (<div className='space-y-5 w-[70%] my-10'>
+              {savedPost.map((item) => (<div className='border border-slate-100 rounded-md'>
+                <PostCard/>
+              </div>))}
+
+            </div>):(
+              <div>Repost</div>
+              )}
+          </div>
+        </section>
+      </div>
+    </Card>
   )
 }
 
