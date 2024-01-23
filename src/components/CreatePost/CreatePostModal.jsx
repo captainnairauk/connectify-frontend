19da774f70be0526a2ae99972970cef7da6d069d
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-01-23 11:43:30 +0530

    Add image,video in post modal

--- src/components/CreatePost/CreatePostModal.jsx
+++ src/components/CreatePost/CreatePostModal.jsx
@@ -1,5 +1,8 @@
-import { Box, Modal, Typography } from '@mui/material'
-import React from 'react'
+import { Avatar, Backdrop, Box, Button, CircularProgress, IconButton, Modal} from '@mui/material'
+import { Formik, useFormik } from 'formik';
+import React, { useState } from 'react'
+import ImageIcon from '@mui/icons-material/Image';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
 
 const style = {
   position: 'absolute',
@@ -15,6 +18,28 @@ const style = {
 };
 
 const CreatePostModal = ({open, handleClose}) => {
+
+  const [selectedImage, setSelectedImage] = useState();
+  const [selectedVideo, setSelectedVideo] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSelectImage = () =>{
+
+  }
+
+  const handleSelectVideo = () => {
+
+  }
+  const formik = useFormik({
+    initialValues:{
+      caption:"",
+      image:"",
+      video:""
+    },
+    onSubmit:(values) => {
+      console.log("formik values", values);
+    }
+  });
   return (
     <Modal
         open={open}
@@ -23,12 +48,63 @@ const CreatePostModal = ({open, handleClose}) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <form onSubmit={formik.handleSubmit}>
+            <div>
+              <div className='flex space-x-4 items-center'>
+                <Avatar/>
+                <div className=''>
+                  <p className='font-bold text-lg'>User name</p>
+                  <p className='text-sm'>@user_name</p>
+                </div>
+              </div>
+              <textarea
+              className='outline-none w-full mt-5 p-2 bg bg-transparent border
+              border-[#3b4054] rounded-sm'
+              placeholder="write caption..."
+              name="caption"
+              onChange={formik.handleChange}
+              value={formik.values.caption}
+              id="" rows="4">
+              </textarea>
+              <div className='flex space-x-5 items-center mt-5'>
+                  <div>
+                    <input type="file" accept='image/*'
+                    onChange={handleSelectImage} style={{display: "none"}}
+                    id='image-input'/>
+                    <label htmlFor="image-input">
+                      <IconButton color='primary'>
+                        <ImageIcon/>
+                      </IconButton>
+                    </label>
+                    <span>Image</span>
+                  </div>
+
+                  <div>
+                    <input type="file" accept='video/*'
+                    onChange={handleSelectVideo} style={{display: "none"}}
+                    id='video-input'/>
+                    <label htmlFor="video-input">
+                      <IconButton color='primary'>
+                        <VideoCallIcon/>
+                      </IconButton>
+                    </label>
+                    <span>Video</span>
+                  </div>
+              </div>
+
+              {selectedImage && <div>
+                <img className= "h-[10rem]"src={selectedImage} alt="" />
+              </div>}
+
+              <div className='flex w-full justify-end'>
+                <Button variant='contained' type='submit' sx={{borderRadius: "1.5rem"}}>Post</Button>
+              </div>
+            </div>
+          </form>
+          <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={isLoading} onClick={handleClose}>
+              <CircularProgress color="inherit" />
+          </Backdrop>
         </Box>
     </Modal>
   )
